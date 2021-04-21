799b431462bf8e50d8330b5d561b37bd07287940
María Rozo <u20152142814@usco.edu.co>
2021-04-21 13:24:37 -0500

    Adding Book Details exercise - Scanner class exercise

---
+++ Fundamentos/HolaMundoJava/src/EjemploLibro.java
@@ -0,0 +1,20 @@
+// Clase EjemploLibro
+
+import java.util.Scanner;
+
+public class EjemploLibro {
+    public static void main(String args[]){
+        Scanner consola = new Scanner(System.in);
+        
+        // Solicitando título
+        System.out.println("Proporciona el título: ");
+        var titulo = consola.nextLine();
+        
+        // Solicitando autor
+        System.out.println("Proporciona el autor");
+        var autor = consola.nextLine();
+        
+        // Imprimiendo resultado
+        System.out.println(titulo + " fue escrito por " + autor);
+    }
+}
