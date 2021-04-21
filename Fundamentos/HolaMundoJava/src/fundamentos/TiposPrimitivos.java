d6d64358b3b3f89aa6a0969557f2bc47e26de699
María Rozo <u20152142814@usco.edu.co>
2021-04-21 14:17:47 -0500

    Adding Primitive Java variable types

---
+++ Fundamentos/HolaMundoJava/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+// Clase TiposPrimitivos
+
+public class TiposPrimitivos {
+    public static void main(String args[]){
+        /*
+            Tipos primitivos enteros: byte, short, int, long
+        */
+        // byte numeroByte = 10;
+        byte numeroByte = (byte)129;
+        System.out.println(numeroByte);
+        System.out.println("Valor mínimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máximo byte: " + Byte.MAX_VALUE);
+    }
+}
