9b1674553f105656887522d9ebcad7ef0a35600d
María Rozo <u20152142814@usco.edu.co>
2021-04-21 10:46:15 -0500

    First commit - Udemy Java University course

---
+++ Fundamentos/HolaMundoJava/src/EnterosString.java
@@ -0,0 +1,16 @@
+
+public class EnterosString {
+    public static void main(String args[]) {
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //Modificando el valor de la variable
+        miVariableEntera=5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena="Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena="Adiós";
+        System.out.println(miVariableCadena);
+    }
+}
