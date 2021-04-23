7e0cdddaac3be3560bde2246ea51d24cdc1eda18
María Rozo <u20152142814@usco.edu.co>
2021-04-23 18:50:15 -0500

    Adding some IfElse conditionals examples

--- Fundamentos/HolaMundoJava/src/section5/SentenciaIfElse.java
+++ Fundamentos/HolaMundoJava/src/section5/SentenciaIfElse.java
@@ -1,14 +1,44 @@
 package section5;
 
+import java.util.Scanner;
+
 public class SentenciaIfElse {
     public static void main(String args[]){
-        var condicion = true;
         
-        // Ejemplo sencillo de condición verdadera o falsa
-        if(condicion) {
-            System.out.println("Condición verdadera");
+        // Ejemplos de Sentencia Interactivos - creando objeto Scanner
+        var scanner = new Scanner(System.in);
+        
+        // Ejemplo 1: condición simple
+        System.out.println("EJERCICIO DE CONDICIÓN SIMPLE: true O false");
+        System.out.print("Insertar condición (true/false): ");
+        var condicion = Boolean.parseBoolean(scanner.nextLine());
+        if(condicion) { // por defecto evalúa la condicion con " == true"
+            System.out.println("Condición verdadera.");
         } else {
-            System.out.println("Condición falsa");
+            System.out.println("Condición falsa.");
         }
+        
+        // Ejemplo 2: condición anidada
+        // Buena práctica: anidar todo con if-else en vez de if's separados
+        System.out.println("EJERCICIO 2: CONDICIONAL IF-ELSE - EVALUAR SI NÚMERO SE ENCUENTRA DEL 1 AL 4");
+        System.out.print("Insertar número cualquiera: ");
+        var numero = Integer.parseInt(scanner.nextLine());
+        var numeroTexto = "";
+        
+        // Condicion if-else anidada
+        if (numero == 1) {
+            numeroTexto = "uno";
+        } else if (numero == 2) {
+            numeroTexto = "dos";
+        } else if (numero == 3) {
+            numeroTexto = "tres";
+        } else if (numero == 4) {
+            numeroTexto = "cuatro";
+        } else {
+            numeroTexto = "Número no encontrado";
+        }
+        
+        // Imprimiendo resultado
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
