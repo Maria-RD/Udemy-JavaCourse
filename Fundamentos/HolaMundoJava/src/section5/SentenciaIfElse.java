0b0dba9c596f8eef887092fcb5fadcdc1cfb72cd
María Rozo <u20152142814@usco.edu.co>
2021-04-23 18:01:03 -0500

    Adding a short example of If-Else sentence

---
+++ Fundamentos/HolaMundoJava/src/section5/SentenciaIfElse.java
@@ -0,0 +1,14 @@
+package section5;
+
+public class SentenciaIfElse {
+    public static void main(String args[]){
+        var condicion = true;
+        
+        // Ejemplo sencillo de condición verdadera o falsa
+        if(condicion) {
+            System.out.println("Condición verdadera");
+        } else {
+            System.out.println("Condición falsa");
+        }
+    }
+}
