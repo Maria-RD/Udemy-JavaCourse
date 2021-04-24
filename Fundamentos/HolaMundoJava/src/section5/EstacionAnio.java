063a12df0dd1f2ee30e062bdf84e17e666cbf0ab
María Rozo <u20152142814@usco.edu.co>
2021-04-23 19:04:37 -0500

    Adding example if-else statement: seasons of the year

---
+++ Fundamentos/HolaMundoJava/src/section5/EstacionAnio.java
@@ -0,0 +1,28 @@
+package section5;
+
+import java.util.Scanner;
+
+public class EstacionAnio {
+    public static void main(String args[]){
+        // Ejercicio dinámico - Creando objeto de la clase Scanner
+        var scanner = new Scanner(System.in);
+        System.out.println("CÁLCULO ESTACIÓN DEL AÑO - if-else");
+        System.out.print("Inserte número del mes: ");
+        var mes = Integer.parseInt(scanner.nextLine());
+        var estacion = "Estación desconocida";
+        
+        // Condicional if-else anidada
+        if (mes == 1 || mes == 2 || mes == 12){
+            estacion = "Invierno";
+        } else if (mes == 3 || mes == 4 || mes == 5 ) {
+            estacion = "Primavera";
+        } else if (mes == 6 || mes == 7 || mes == 8 ) {
+            estacion = "Verano";
+        } else if ( mes == 9 || mes == 10 || mes == 11) {
+            estacion = "Otoño";
+        }
+        
+        // Imprimiendo resultado
+        System.out.println("estacion = " + estacion);
+    }
+}
